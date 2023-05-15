35f901547fcbcd25d42ac5f923f71eb683393a1f
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-05-15 22:57:13 +0200

    create dto

--- RubricaManager-FE-Angular/src/app/models/user.ts
+++ RubricaManager-FE-Angular/src/app/models/user.ts
@@ -1,4 +1,20 @@
 export class User{
   username: string = "";
-  password: string = "";
+  email? : string;
+  name? : string;
+  lastname? : string;
+}
+
+export class UserRegisterDTO{
+  username : string = "";
+  password : string = "";
+  confirmPassword : string = "";
+  email : string = "";
+  name? : string;
+  lastname? : string;
+}
+
+export class UserLoginDTO{
+  username : string = "";
+  password : string = "";
 }
