8059ee892fc7c83e0b1690e9a049dfaa4804e12b
gdemartino93 <113302882+gdemartino93@users.noreply.github.com>
2023-05-16 01:41:50 +0200

    register page with form and dto

--- RubricaManager-FE-Angular/src/app/pages/auth/register/register.component.ts
+++ RubricaManager-FE-Angular/src/app/pages/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { UserRegisterDTO } from 'src/app/models/user';
+import { FormControl, FormGroup } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent {
+  constructor(private auth: AuthService) { }
+
+  user : UserRegisterDTO = new UserRegisterDTO();
+  formRegister = new FormGroup(
+    {
+      username: new FormControl(''),
+      password: new FormControl(''),
+      confirmPassword: new FormControl(''),
+      email: new FormControl(''),
+      name: new FormControl(''),
+      lastname: new FormControl('')
+    });
+    register(){
+      this.auth.register(this.user).subscribe();
+    }
+
 
 }
